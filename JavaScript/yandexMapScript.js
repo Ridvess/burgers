5b0049f5b028e1917e13e3785ed3ce3146ebaf10
Ridves <phillroman24@gmail.com>
2019-06-08 16:05:02 +0300

    added scripts

--- JavaScript/yandexMapScript.js
+++ JavaScript/yandexMapScript.js
@@ -1,6 +1,39 @@
 ymaps.ready(init);
 
-var placemarks =
+var placemarks = [
+    {
+        latitude: 59.97,
+        longitude: 30.31,
+        hintContent: '<div class="map__hint">ул. Литераторов, д.19</div>',
+        balloonContent: [
+            '<div class="map__balloon">',
+            'Самые вкусные бургеры у нас!',
+            '</div>'
+        ]
+    },
+    {
+        latitude: 59.94,
+        longitude: 30.25,
+        hintContent: '<div class="map__hint">Малый проспект В О, д. 64</div>',
+        balloonContent: [
+            '<div class="map__balloon">',
+            'Самые вкусные бургеры у нас!',
+            '</div>'
+        ]
+    },
+    {
+        latitude: 59.93,
+        longitude: 30.34,
+        hintContent: '<div class="map__hint">наб. реки Фонтанки, д. 56</div>',
+        balloonContent: [
+            '<div class="map__balloon">',
+            'Самые вкусные бургеры у нас!',
+            '</div>'
+        ]
+    }
+];
+
+var geoObjects= [];
 
 function init() {
     var map = new ymaps.Map("mapContainer", {
@@ -10,34 +43,24 @@ function init() {
         behaviors: ['drag']
     });
 
-    var placemark = new ymaps.Placemark([59.97, 30.31], {
-        hintContent: '<div class="map__hint">ул. Литераторов, д.19</div>',
-    },
-    {
-        iconLayout: 'default#image',
-        iconImageHref: 'img/icons/map-marker.svg',
-        iconImageSize: [46,58],
-        iconImageOffset: [-23,-57]
+    for(var i = 0; i < placemarks.length; i++){
+        geoObjects[i] = new ymaps.Placemark([placemarks[i].latitude, placemarks[i].longitude], {
+                hintContent: placemarks[i].hintContent,
+                balloonContent: placemarks[i].balloonContent.join('')
+            },
+            {
+                iconLayout: 'default#image',
+                iconImageHref: 'img/icons/map-marker.svg',
+                iconImageSize: [46,58],
+                iconImageOffset: [-23,-57]
+            });
+    }
+
+    var clusterer = new ymaps.Clusterer({
+
     });
-    var placemark1 = new ymaps.Placemark([59.96, 30.31], {
-        hintContent: '<div class="map__hint">ул. Литераторов, д.19</div>',
-    },
-    {
-        iconLayout: 'default#image',
-        iconImageHref: 'img/icons/map-marker.svg',
-        iconImageSize: [46,58],
-        iconImageOffset: [-23,-57]
-    });
-    var placemark2 = new ymaps.Placemark([59.99, 30.31], {
-        hintContent: '<div class="map__hint">ул. Литераторов, д.19</div>',
-    },
-    {
-        iconLayout: 'default#image',
-        iconImageHref: 'img/icons/map-marker.svg',
-        iconImageSize: [46,58],
-        iconImageOffset: [-23,-57]
-    });
-    map.geoObjects.add(placemark);
-    map.geoObjects.add(placemark1);
-    map.geoObjects.add(placemark2);
+
+    map.geoObjects.add(clusterer);
+    //map.geoObjects.add(placemark);
+    clusterer.add(geoObjects);
 }
