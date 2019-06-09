065e4d3ca6cf9ce5e9e74dc0f581c0b075a41882
Ridves <phillroman24@gmail.com>
2019-06-09 21:06:37 +0300

    fixing stuff

--- JavaScript/accoMenuBurgers.js
+++ JavaScript/accoMenuBurgers.js
@@ -8,15 +8,22 @@ $(accoItem1).on("click", function (e) {
 
     if(!$(this).hasClass('accordeon__item-active')){
         $('ul.menu-acco li ').removeClass('active');
-        // this.style.position = "absolute";
-        // this.style.zIndex = "10";
         $(this).addClass('active');} else {
         $(this).removeClass('active');
-        // this.style.position = "relative";
-        // this.style.zIndex = "0";
     }
 });
 
+
+// if(!$(this).hasClass('accordeon__item-active')){
+//     $('ul.menu-acco li ').removeClass('active');
+//     // this.style.position = "absolute";
+//     // this.style.zIndex = "10";
+//     $(this).addClass('active');} else {
+//     $(this).removeClass('active');
+//     // this.style.position = "relative";
+//     // this.style.zIndex = "0";
+// }
+// });
 
 // $(accoClose).on("click", e=>{
 //
